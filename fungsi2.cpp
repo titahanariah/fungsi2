0adc6a1ed56883cbe8ac7e78218ae0dc04198ccc
titahanariah <titahanariah@users.noreply.github.com>
2017-03-31 23:22:06 +0700

    Add files via upload

---
+++ fungsi2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+bool genap (int);
+int main (){
+
+	cout<<"hasilnya adalah:" <<genap(10);
+	return 0;
+	}
+	bool genap (int n) {
+	
+	return ( n % 2 ==0);
+	}
+	
